7af0fb56d509eaaa27c109fc1d4f6510227f5a89
Zayyni <127606083+Zayyni@users.noreply.github.com>
2023-04-29 16:58:15 +0500

    Pairs in Array using Java

---
+++ Pairs_in_Array.java
@@ -0,0 +1,16 @@
+public class Pairs_in_Array {
+    public static void printPairs(int numbers[]){
+        for(int i=0;i<numbers.length;i++){
+            int curr=numbers[i];
+            for(int j=i+1;j<numbers.length;j++){
+                System.out.print("(" + curr +"," + numbers[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        printPairs(numbers);
+    }
+}
