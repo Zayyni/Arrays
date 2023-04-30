01832331b61947f9832839c6f5477299ae606997
Zayyni <127606083+Zayyni@users.noreply.github.com>
2023-04-30 16:29:22 +0500

    Print a SubArray

---
+++ Print_Sub_Arrays.java
@@ -0,0 +1,23 @@
+public class Print_Sub_Arrays {
+    
+    public static void printSubarrays(int numbers[]){
+        int ts=0;
+        for(int i=0;i<numbers.length;i++){
+            int start = i;
+            for(int j=i; j <numbers.length;j++){
+                int end = j;
+                for(int k=start;k<=end;k++){//print
+                    System.out.print(numbers[k]+ " ");//subarray
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total Subarrays "+ts);
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+        printSubarrays(numbers);
+    }
+}
